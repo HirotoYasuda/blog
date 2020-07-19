f2f0b7d07ded33b4d8e7a1a617394a744f1877fd
HirotoYasuda <qnjmr202@yahoo.co.jp>
2020-07-19 10:58:33 +0000

    active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  before_create :default_image
+
+    def default_image
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'avatar-default.png')), filename: 'avatar-dafault.png', content_type: 'image/png')
+    end
 end
